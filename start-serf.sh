9a2068bafca8d80958646f04c5686b84e38ddd10
hawthorn <patrik.hagedorn@gmx.de>
2015-07-23 17:47:09 +0200

    Fixed file path to serf snapshot.

--- start-serf.sh
+++ start-serf.sh
@@ -4,4 +4,4 @@ SNAPSHOT_DIR="/opt/serf"
 
 mkdir -p ${SNAPSHOT_DIR}
 
-exec serf agent -replay -rejoin -snapshot=${SNAPSHOT_DIR} -tag role=${SERF_ROLE} -join=${LOADBALANCER_1_PORT_7946_TCP_ADDR}:${LOADBALANCER_1_PORT_7946_TCP_PORT}
+exec serf agent -replay -rejoin -snapshot=${SNAPSHOT_DIR}/snapshot -tag role=${SERF_ROLE} -join=${LOADBALANCER_1_PORT_7946_TCP_ADDR}:${LOADBALANCER_1_PORT_7946_TCP_PORT}
