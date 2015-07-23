067f56120a71736e5e9e7e7d4e736a6ca4db07e6
hawthorn <patrik.hagedorn@gmx.de>
2015-07-23 16:43:04 +0200

    Added -replay -rejoin and -snapshot options to serf agent execution.

--- start-serf.sh
+++ start-serf.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-exec serf agent -tag role=${SERF_ROLE} -join=${LOADBALANCER_1_PORT_7946_TCP_ADDR}:${LOADBALANCER_1_PORT_7946_TCP_PORT}
+SNAPSHOT_DIR="/opt/serf"
+
+mkdir -p ${SNAPSHOT_DIR}
+
+exec serf agent -replay -rejoin -snapshot=${SNAPSHOT_DIR} -tag role=${SERF_ROLE} -join=${LOADBALANCER_1_PORT_7946_TCP_ADDR}:${LOADBALANCER_1_PORT_7946_TCP_PORT}
