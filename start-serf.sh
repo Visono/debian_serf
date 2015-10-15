b1f480dc07161da3fa8401bee96a244743796388
hawthorn <patrik.hagedorn@gmx.de>
2015-10-15 18:26:33 +0200

    Added support for external serf cluster.

--- start-serf.sh
+++ start-serf.sh
@@ -10,4 +10,9 @@ SNAPSHOT_DIR="/opt/serf"
 
 mkdir -p ${SNAPSHOT_DIR}
 
-exec serf agent -replay -rejoin -snapshot=${SNAPSHOT_DIR}/snapshot -tag role=${SERF_ROLE} -join=${LOADBALANCER_1_PORT_7946_TCP_ADDR}:${LOADBALANCER_1_PORT_7946_TCP_PORT}
+if [ "x${SERF_CLUSTER}" != "x" ]; then
+    exec serf agent -replay -rejoin -snapshot=${SNAPSHOT_DIR}/snapshot -tag role=${SERF_ROLE} -join=${SERF_CLUSTER}:7946
+else
+    exec serf agent -replay -rejoin -snapshot=${SNAPSHOT_DIR}/snapshot -tag role=${SERF_ROLE} -join=${LOADBALANCER_1_PORT_7946_TCP_ADDR}:${LOADBALANCER_1_PORT_7946_TCP_PORT}
+fi
+
