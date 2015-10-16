1e538113632f67219e31e5dba833baca674b5265
hawthorn <patrik.hagedorn@gmx.de>
2015-10-16 14:05:00 +0200

    Added support for external serf cluster. Fixed bug in variable substitution.

--- start-serf.sh
+++ start-serf.sh
@@ -9,5 +9,5 @@ SNAPSHOT_DIR="/opt/serf"
 
 mkdir -p ${SNAPSHOT_DIR}
 
-exec serf agent -replay -rejoin -snapshot=${SNAPSHOT_DIR}/snapshot -iface=${SERF_IFACE:-"eth0"} -tag role=${SERF_ROLE} -join=${SERF_CLUSTER:-$LOADBALANCER_1_PORT_7946_TCP_ADDR}:${SERF_PORT:-7946-$LOADBALANCER_1_PORT_7946_TCP_PORT}
+exec serf agent -replay -rejoin -snapshot=${SNAPSHOT_DIR}/snapshot -iface=${SERF_IFACE:-"eth0"} -tag role=${SERF_ROLE} -join=${SERF_CLUSTER:-$LOADBALANCER_1_PORT_7946_TCP_ADDR}:${SERF_PORT:-$LOADBALANCER_1_PORT_7946_TCP_PORT}
 
